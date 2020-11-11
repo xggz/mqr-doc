7524fa296d92f8071bfbda86fe4f19b51ff8db49
冬天的秘密 <yyimba@qq.com>
2020-11-11 15:39:43 +0800

    :tada: Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'MQR',
+    description: '茉莉QQ机器人，功能强大，插件灵活/丰富。',
+    themeConfig: {
+        nav: [
+            { text: '指南', link: '/guide' },
+            { text: '配置', link: '/config' },
+            {
+                text: '了解更多', ariaLabel: 'More', items: [
+                    { text: '了解更多一', link: '/more1' },
+                    { text: '了解更多二', link: '/more2' }
+                ]
+            },
+            { text: '更新日志', link: '/change-log' },
+            { text: '论坛', link: 'https://bbs.molicloud.com', target:'_blank' }
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hero.png
+heroText: 茉莉QQ机器人
+tagline: 开始机器人的全新体验
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 功能强大
+  details: 集群管理、群娱乐和工具为一体，支持QQ多种事件和消息格式的处理。
+- title: 智能灵活
+  details: 插件随心扩展，基于插件系统，可以完成各式各样的功能。
+- title: 放心无忧
+  details: 基于Mirai Android协议，可后台运行，机器人全天24小时在线。
+footer: AGPL-3.0 Licensed | Copyright © 2020 茉莉云
+---
